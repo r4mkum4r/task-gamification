d5dc95ea48bb1706aa06b21155a377500d2e8175
raramakrishna <raramakrishna@egencia.com>
2018-08-09 14:26:40 +0530

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { AppNavigationComponent } from './app-navigation/app-navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, AppNavigationComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule],
   providers: [],
   bootstrap: [AppComponent],
